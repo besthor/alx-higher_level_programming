4e3ba79d5ba7849ac689173ba7dc3cc28450b2f5
besthor <igbebestor7@gmail.com>
2022-12-29 04:10:20 -0800

    python-tests_cases

--- python-tests_cases/100-main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-matrix_mul = __import__('100-matrix_mul').matrix_mul
-
-print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
--- python-tests_cases/README.md
+++
@@ -1,1 +0,0 @@
-# Solutions to tasks on Python - Test-driven development
