8011c5428d19a975159b4991612d132b78c43ce7
besthor <igbebestor7@gmail.com>
2023-02-15 06:44:29 -0800

    a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
--- List shows without genre
+-- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 SELECT tv_shows.title, tsg.genre_id
 	FROM tv_shows
 	LEFT JOIN tv_show_genres AS tsg
