0acb1d02889916bd0fda7df9fbe31dc278a964d4
Besthor <111004790+besthor@users.noreply.github.com>
2023-03-14 10:25:04 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // Basic print with Javascript
-
 const arg1 = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < arg1.length; i++) {
   console.log(arg1[i]);
