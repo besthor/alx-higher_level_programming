70db6e6b5553366c1f7c01012d5adeefe1c53f8e
Besthor <111004790+besthor@users.noreply.github.com>
2023-04-27 22:32:28 +0100

    Create 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+# a script that reads and prints the content of a file.
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
