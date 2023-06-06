1f7fb6faf4179635c07c4f764ef39bf1e09b2a98
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-06 22:07:04 +0100

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a DELETE request to the URL passed
+# A script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -s "$1" -X DELETE
