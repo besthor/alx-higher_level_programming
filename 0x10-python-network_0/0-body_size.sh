695fa124c9c692bd447272a8b92ffdea779e72ab
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-06 21:51:20 +0100

    Updated 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# Script that shows the Content-Length from a HTTP request
+# A Bash script that takes in a URL, sends a request to that URL
+# and displays the size of the body of the response
+
 curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
