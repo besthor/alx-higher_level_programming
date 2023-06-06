f78d81992f58d354a71bff4e4a16c43bf52d1816
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-06 22:56:20 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@
 if __name__ == "__main__":
     import urllib.request
     
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
