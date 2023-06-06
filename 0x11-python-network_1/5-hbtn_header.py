ea2c4ddb6c3404b87d090aabe99454acfac8c0cb
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-06 23:14:00 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,9 +6,10 @@ displays the value of a variable in the response header
 import requests
 import sys
 
+
 if __name__ == "__main__":
     try:
         r = requests.get(sys.argv[1])
         print(r.headers['X-Request-Id'])
-        except:
-            pass
+    except:
+        pass
