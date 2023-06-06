a1597f57a2b5c523cf8035d1c1d8bf436d1fd914
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-06 21:52:12 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
+# A Bash script that takes in a URL, sends a request and displays the size of the body of the response
 
 curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
