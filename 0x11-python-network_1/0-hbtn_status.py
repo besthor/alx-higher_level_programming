ef18c2bc79f842edbee8dc9198c7997119b2e2e9
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-06 22:45:39 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status """
-import urllib.request
-
-
+"""Python script that 
+- fetches https://alx-intranet.hbtn.io/status
+- use the package urllib
+"""
 if __name__ == "__main__":
+    import urllib.request
+    
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
