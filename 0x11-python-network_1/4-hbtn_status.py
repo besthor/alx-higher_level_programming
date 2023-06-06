461c573c8b15dbc0ae02d65f19e02586e5f7a8ba
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-06 23:05:39 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     t = r.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
