298cb11f8ef5eac666b137f1f307ec371abbcdc8
besthor <igbebestor@gmail.com>
2023-06-07 02:39:37 -0700

    Web scraping with JavaScript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
-# a script that reads and prints the content of a file.
-
 const fs = require('fs');
-const filename = process.argv[2];
-
-fs.readFile(filename, 'utf-8', (error, content) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(content);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
