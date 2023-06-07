240e453ebe154c7b563480b54643be4771e8b68d
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-07 10:27:30 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 # a script that reads and prints the content of a file.
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, contents) {
-  if (contents === undefined) {
-    console.log(err);
-  } else {
-    console.log(contents);
-  }
-});
+
+function readFile(filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(data);
+  });
+}
+
+
